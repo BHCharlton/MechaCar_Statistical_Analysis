2227e2dbfcec5b16c99bc08fd239cfb32250d977
BHCharlton <bhcharlton@netscape.net>
2022-04-09 15:18:40 -0500

    Deliverable 3 update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,6 @@ lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clea
 head(mechacar_mpg_df)
 
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg_df))
+suspension_coil_df <- read.csv(file = "C:/Users/bhcha/Desktop/Git_Repos/Challenges/Module 15/MechaCar_Statistical_Analysis/Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
