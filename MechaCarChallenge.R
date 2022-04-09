797979f8a247abf5d3830ac0234c3e49da6702b2
BHCharlton <bhcharlton@netscape.net>
2022-04-09 16:41:52 -0500

    Final commit

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,10 @@ summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + gro
 suspension_coil_df <- read.csv(file = "C:/Users/bhcha/Desktop/Git_Repos/Challenges/Module 15/MechaCar_Statistical_Analysis/Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
 total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# TTests on suspension coils
+t.test(suspension_coil_df$PSI, mu = 1500)
+
+t.test(subset(suspension_coil_df, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
